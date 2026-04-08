8b93c77b8f8b5de52e601b13b752e79aaa08d1ea
Marcos Kettermann <marcosk10@yahoo.com.br>
2026-04-08 15:09:52 -0300

    feat: adicionar bootstrap-icons ao projeto e ajustar a estrutura do cabeçalho e navegação

--- src/frontend/korp-frontend/src/app/layout/navegacao/navegacao.spec.ts
+++ src/frontend/korp-frontend/src/app/layout/navegacao/navegacao.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Navegacao } from './navegacao';
+import { provideRouter } from '@angular/router';
 
 describe('Navegacao', () => {
   let component: Navegacao;
@@ -8,9 +9,10 @@ describe('Navegacao', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Navegacao]
+      imports: [Navegacao],
+      providers: [provideRouter([])],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Navegacao);
     component = fixture.componentInstance;
